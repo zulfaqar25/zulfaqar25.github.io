b00a9d3e1f5ad08c88b8ddd9c57a95d66db89b12
zulfaqar25 <57497704+zulfaqar25@users.noreply.github.com>
2019-11-07 23:35:50 +0700

    Add files via upload

---
+++ tentang.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>OK "Order Kantin" </title>
+<link rel="icon" href="image/ok1.jpg" type="image/jpg" sizes="16x14"> 
+</head>
+<body>
+<body style="background-color:powderblue;">
+<h1 align="center">OK "Order Kantin"</h1>
+<marquee>Anda ingan makan? Segera Pesan di Aplikasi Order Kantin</marquee>
+
+<?php 
+$oke=1;
+
+if($oke>2){
+	echo "dia lebih dari dua";
+}else{
+	echo '<div align="center"> <a href="tentang.html"><img src="image/ok1.jpg" alt="Trulli" width="500" height="500"></a>  </div>';
+}
+
+?>
+
+
+
+</body>
+</html>
